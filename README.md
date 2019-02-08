2f37b797834e6de44f8bcbd282a0bae1e9c1d9a4
Tamas Piros <tpiros@tpiros-macbook.local>
2019-02-08 12:15:01 +0100

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Adaptive image caching based on network speed with Workbox.js
+
+In order to install the dependencies please execute `npm i` from the project folder.
+
+Once the dependencies have been installed, please run `npm run build`.
+
+Once the build is complete you can find the final, built project under the `build` folder.
+
+## Serve the application
+In order to serve the contents of the build folder and to see the final solution execute `npm start`.
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const del = require('del');
+const workboxBuild = require('workbox-build');
+
+const clean = () => del(['build/*'], { dot: true });
+gulp.task('clean', clean);
+
+const copy = () => gulp.src(['app/**/*']).pipe(gulp.dest('build'));
+gulp.task('copy', copy);
+
+const copyCloudinaryPlugin = () => gulp.src(['node_modules/cloudinary-workbox-plugin/dist/cloudinaryPlugin.js']).pipe(gulp.dest('build'));
+gulp.task('copy-cloudinary-plugin', copyCloudinaryPlugin);
+
+const serviceWorkerBuild = async () => {
+  return await workboxBuild
+  .generateSW({
+    swDest: 'build/sw.js',
+    globDirectory: 'build',
+    importScripts: ['./cloudinaryPlugin.js'],
+    runtimeCaching: [{
+      urlPattern: '/api/news',
+      handler: 'staleWhileRevalidate',
+      options: {
+        cacheName: 'api-cache',
+      }
+    }, {
+      urlPattern: new RegExp('^https:\/\/res\.cloudinary\.com\/.*\/image\/upload\/'),
+      handler: 'cacheFirst',
+      options: {
+        cacheName: 'cloudinary-images',
+        plugins: [{
+          requestWillFetch: async ({ request }) => cloudinaryPlugin.requestWillFetch(request)
+        }]
+      }
+    }]
+  });
+};
+
+
+gulp.task('service-worker-importscript', serviceWorkerBuild);
+
+const build = gulp.series('clean', 'copy', 'copy-cloudinary-plugin', 'service-worker-importscript');
+
+const watch = () => gulp.watch('app/**/*', build);
+
+gulp.task('watch', watch);
+gulp.task('default', build);
